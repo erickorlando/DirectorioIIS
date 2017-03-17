4953fc3aa8dc99753a87897b1a8c3138a59e8f5d
Erick Orlando <erickorlando@outlook.com>
2017-03-17 17:16:55 -0500

    Leer un archivo de un directorio virtual de IIS

--- DirectorioIIS/Module1.vb
+++ DirectorioIIS/Module1.vb
@@ -1,6 +1,35 @@
-﻿Module Module1
+﻿Imports System.Net
+
+Module Module1
 
     Sub Main()
+
+        Try
+            Dim ruta = "http://localhost/aspnet_client"
+
+            Dim archivo As String = ruta + "/readme.txt"
+
+            Console.WriteLine(archivo)
+
+            Dim client = WebRequest.Create(archivo)
+            client.Method = "GET"
+
+            Dim result = client.GetResponse()
+
+            If result.ContentLength > 0 Then
+
+                Console.WriteLine("Archivo existe")
+
+            Else
+                Console.WriteLine("No encontrado")
+
+            End If
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        Finally
+            Console.ReadLine()
+        End Try
+
 
     End Sub
 
